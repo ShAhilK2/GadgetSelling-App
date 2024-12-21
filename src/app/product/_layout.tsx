8bc718c67172310482747d944f1b90a73a831ed6
ShAhilK2 <shahil@Shahils-MacBook-Air.local>
2024-12-21 13:17:48 +0530

    Shop Product and Categories

--- src/app/product/_layout.tsx
+++ src/app/product/_layout.tsx
@@ -5,7 +5,7 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function ProductLayout(){
     return <Stack >
-        <Stack.Screen name="index" options={({navigation})=>({
+        <Stack.Screen name="[slug]" options={({navigation})=>({
              headerShown:true,
              headerLeft: ()=><TouchableOpacity onPress={()=> navigation.goBack()}>
                  <Ionicons name="arrow-back" size={24} color="black"/>
